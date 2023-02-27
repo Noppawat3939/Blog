88ec90cb58f2490c15dcb9c00a9f256d11ab53db
GGOPGAP <noppawat@magicboxsolution.com>
2023-02-27 13:09:59 +0700

    add api

--- server/api/index.js
+++ server/api/index.js
@@ -1,14 +1,3 @@
-const app = require("express")();
+const server = require("../server");
 
-app.get("/api", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: start`);
-});
-
-app.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
-
-module.exports = app;
+module.exports = server;
